a785fe234ef95fb57cb49361947d427dc40ab85f
Han Zhu <zhuhan568@outlook.com>
2024-01-07 20:28:37 -0500

    change to 1 degree refinement

--- VESPER_code/mrc.h
+++ VESPER_code/mrc.h
@@ -1,142 +1,145 @@
-typedef struct{
+typedef struct
+{
 	char filename[LIN];
 	double map_t;
 	int Nthreads;
-	int xdim,ydim,zdim;
-	int ncstart,nrstart,nsstart;
-	int mx,my,mz;
-	float xlen,ylen,zlen;
-	float alpha,beta,gamma;
-	int mapc,mapr,maps;
-	float dmin,dmax,dmean;
+	int xdim, ydim, zdim;
+	int ncstart, nrstart, nsstart;
+	int mx, my, mz;
+	float xlen, ylen, zlen;
+	float alpha, beta, gamma;
+	int mapc, mapr, maps;
+	float dmin, dmax, dmean;
 	int ispg;
 	int nsymbt;
 	float orgxyz[3];
 	int NumVoxels;
-	float *dens,*sco;
+	float *dens, *sco;
 	double **vec;
-	float widthx,widthy,widthz;
+	float widthx, widthy, widthz;
 	unsigned int Nact;
 	double cent[3];
-	double dmax2,dsum,std,ave;
+	double dmax2, dsum, std, ave;
 	double std_norm_ave;
 } MRC;
 
-
-typedef struct{
- double **cd,*dens;
- int **origrid;
- int Ncd,Nori;
- int *member;
- float *mask;
+typedef struct
+{
+	double **cd, *dens;
+	int **origrid;
+	int Ncd, Nori;
+	int *member;
+	float *mask;
 } POINTS;
 
-typedef struct{
- double d;
- double dens;
- int id1,id2,eid;
- bool mst,local,keep;
+typedef struct
+{
+	double d;
+	double dens;
+	int id1, id2, eid;
+	bool mst, local, keep;
 } EDGE;
 
-typedef struct{
- int N;
- EDGE *e[20];//!!!!! 10 is not enough
- int cid;//chain
+typedef struct
+{
+	int N;
+	EDGE *e[20]; //!!!!! 10 is not enough
+	int cid;	 // chain
 } NODE;
 
-typedef struct{
- int cut_id,add_id;
- double score;
- //float score;
+typedef struct
+{
+	int cut_id, add_id;
+	double score;
+	// float score;
 } MOVE;
 
+typedef struct
+{
+	double len, bf_len;
+	int Nnode, Ne;
+	NODE *node;
+	int Ntotal, Etotal;
+	int St, Ed;
+	bool *ActE; // Active edge
+	bool *ActN; // Active node
+	// bool *MstE://MST edge
+	int *stock, Nstock;
+	int *nextv;
+	double *cost;
+	int *cid;
+	int *CutTbl, Ncut; // Edge table for cut;
+	int *AddTbl, Nadd; // Edge table for add;
 
-
-typedef struct{
- double len,bf_len;
- int Nnode,Ne;
- NODE *node;
- int Ntotal,Etotal;
- int St,Ed;
- bool *ActE;//Active edge
- bool *ActN;//Active node
- //bool *MstE://MST edge
- int *stock,Nstock;
- int *nextv;
- double *cost;
- int *cid;
- int *CutTbl,Ncut;//Edge table for cut;
- int *AddTbl,Nadd;//Edge table for add;
-
- MOVE *mv; //Movement
- int Nmv;
- int *Path,Lpath;
- double score;
+	MOVE *mv; // Movement
+	int Nmv;
+	int *Path, Lpath;
+	double score;
 } TREE;
 
-typedef struct{
- EDGE *edge;//All edge data
- int Ne;
- bool **adj;
- NODE *node;//All connections
- int Nnode;
- int *cid;
+typedef struct
+{
+	EDGE *edge; // All edge data
+	int Ne;
+	bool **adj;
+	NODE *node; // All connections
+	int Nnode;
+	int *cid;
 
- int Nt;
- EDGE **tree;
+	int Nt;
+	EDGE **tree;
 
- //TREE mst;
+	// TREE mst;
 
 } GRAPH;
 
-
-bool readmrc(MRC *,char *);
+bool readmrc(MRC *, char *);
 bool ToCubic(MRC *);
-bool upsampling(MRC *,double);
+bool upsampling(MRC *, double);
 void out_situs(MRC *);
-bool meanshift(MRC *,POINTS *);
-bool fastLDP(MRC *,POINTS *);
+bool meanshift(MRC *, POINTS *);
+bool fastLDP(MRC *, POINTS *);
 
-bool fastVEC(MRC *,MRC *);
-void SetUpVoxSize(MRC *,MRC *,double,double);
+bool fastVEC(MRC *, MRC *);
+void SetUpVoxSize(MRC *, MRC *, double, double);
 void ShowVec(MRC *);
-void ShowVec2(MRC *,MRC *,int [3]);
-void ShowVec3(MRC *,MRC *,int [3]);
+void ShowVec2(MRC *, MRC *, int[3]);
+void ShowVec3(MRC *, MRC *, int[3]);
 void ShowMRC(MRC *);
 
-double meanshift_pos(MRC *,double [3]);
-bool MergePoints(MRC *,POINTS *);
-bool MergeLDP(MRC *,POINTS *);
-void ShowModel(MRC *,POINTS *);
-void ShowLDP(MRC *,POINTS *);
-void ShowOri(MRC *,POINTS *);
+double meanshift_pos(MRC *, double[3]);
+bool MergePoints(MRC *, POINTS *);
+bool MergeLDP(MRC *, POINTS *);
+void ShowModel(MRC *, POINTS *);
+void ShowLDP(MRC *, POINTS *);
+void ShowOri(MRC *, POINTS *);
 void ShowGraph(GRAPH *);
-void ShowTree(GRAPH *,TREE *);
-void ShowPath(MRC *,POINTS *,GRAPH *,TREE *,int n);
-void ShowPath2(MRC *,POINTS *,GRAPH *,TREE *,int n);
+void ShowTree(GRAPH *, TREE *);
+void ShowPath(MRC *, POINTS *, GRAPH *, TREE *, int n);
+void ShowPath2(MRC *, POINTS *, GRAPH *, TREE *, int n);
 
-bool SetUpGraph(POINTS *, GRAPH *,MRC *,TREE *);
-bool Tabu(GRAPH *,TREE *,TREE *);
-bool InitTree(TREE *,bool);
-bool ConstTree(TREE *,GRAPH *);
+bool SetUpGraph(POINTS *, GRAPH *, MRC *, TREE *);
+bool Tabu(GRAPH *, TREE *, TREE *);
+bool InitTree(TREE *, bool);
+bool ConstTree(TREE *, GRAPH *);
 double QualityTree(GRAPH *, TREE *);
-bool CopyTree(TREE *,TREE *,bool);
-bool SetCutTbl(GRAPH *,TREE *,int *,int,double);
-bool MoveTree(GRAPH *, TREE *, int,int);
+bool CopyTree(TREE *, TREE *, bool);
+bool SetCutTbl(GRAPH *, TREE *, int *, int, double);
+bool MoveTree(GRAPH *, TREE *, int, int);
 
-int CutTree(GRAPH *, TREE *,double,int *,int);
+int CutTree(GRAPH *, TREE *, double, int *, int);
 int AddEdge(GRAPH *, TREE *);
-int SplitChain(GRAPH *, TREE *,int);
+int SplitChain(GRAPH *, TREE *, int);
 
 double RandDouble();
 int RandInt(int);
 void ShuffleTbl(int *, int);
 void ShuffleMv(MOVE *, int);
 
-double Kendall(int *,int,int *,int);
+double Kendall(int *, int, int *, int);
 
-bool PairExhaust(GRAPH *,TREE *,TREE *);
-int ListStEd(GRAPH *,TREE *,int *);
+bool PairExhaust(GRAPH *, TREE *, TREE *);
+int ListStEd(GRAPH *, TREE *, int *);
 
-double OptPath2points(GRAPH *,TREE *,TREE *,int,int,int);
-bool get_path(GRAPH *,TREE *, int, int);
+double OptPath2points(GRAPH *, TREE *, TREE *, int, int, int);
+bool get_path(GRAPH *, TREE *, int, int);
--- VESPER_code/mrcfft.h
+++ VESPER_code/mrcfft.h
@@ -1,16 +1,17 @@
 
 
-typedef struct{
- int t[3];
- double r[3];
- int code;
- double sco;
+typedef struct
+{
+    int t[3];
+    double r[3];
+    int code;
+    double sco;
 } TBL;
 
-bool SearchMAPfft(MRC *,MRC *,double);
-bool SearchMAPfftMT(MRC *,MRC *,double,bool);
-bool SearchMAPfftMT_OVCC(MRC *,MRC *,double,int,bool); //Overlap or CCC
-double GetScore(MRC *, MRC *, int [3]);
-double GetScore2(MRC *, MRC *, int [3], double [5]);
+bool SearchMAPfft(MRC *, MRC *, double);
+bool SearchMAPfftMT(MRC *, MRC *, double, bool);
+bool SearchMAPfftMT_OVCC(MRC *, MRC *, double, int, bool); // Overlap or CCC
+double GetScore(MRC *, MRC *, int[3]);
+double GetScore2(MRC *, MRC *, int[3], double[5]);
 
-void PrintTbl(TBL *,int,MRC *,MRC *,double,double);
+void PrintTbl(TBL *, int, MRC *, MRC *, double, double);
